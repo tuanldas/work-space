4446adfc6d1759ab3d92a3e36611f961724ee383
tuanldas <tuanldas@gmail.com>
2024-07-01 13:58:40 +0700

    Cập nhật folder media

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+        port: 3000,
+        host: true,
+    },
+  build: {
+    chunkSizeWarningLimit: 3000,
+  },
 })
