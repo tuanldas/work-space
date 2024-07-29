ecad766aab3574b3e2b545bc9c20c247d9f1c476
Tuanldas <tuanldas@gmail.com>
2024-07-29 13:35:06 +0700

    Thêm icon ảnh

--- src/pages/Project/Detail/index.tsx
+++ src/pages/Project/Detail/index.tsx
@@ -1,7 +1,15 @@
+import {toAbsoluteUrl} from '../../../helpers/AssetHelpers.ts';
+
 const ProjectDetail = () => {
     return (
         <>
-            <p>asdf</p>
+            <div className="card">
+                <div className="card-body">
+                    <div className="flex justify-center items-center flex-shrink-0 bg-secondary rounded h-[100px] lg:w-[150px] lg:h-[150px] me-7 mb-4">
+                        <img className='max-w-[50px] lg:max-w-[75px] w-[100px]' src={toAbsoluteUrl('media/brand-logos/airbnb-2.svg')} alt={'#'}/>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
